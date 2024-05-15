037877ce996958c670af4972c392a0ba91ade9c3
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-05-15 16:52:28 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        
+        public IActionResult Index()
+        {
+
+            var pizzas = dbContext.Pizza 
+            return View();
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+
+    [Table("Pizze")]
+    public class Pizza
+    {
+        [Key]public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string PhotoURL { get; set; }
+
+        public decimal Price { get; set; }
+
+
+        public Pizza() { }
+
+        public Pizza(string name, string description, string photo, decimal price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.PhotoURL = photo;
+            this.Price = price;
+        }
+    }
+}
