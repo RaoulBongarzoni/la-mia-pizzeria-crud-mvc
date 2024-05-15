17a5e8d852eb1364e11480af75973e7a685ce1ee
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-05-15 18:08:54 +0200

    Controllers and pizza Details

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_static.Controllers
@@ -9,9 +10,34 @@ namespace la_mia_pizzeria_static.Controllers
         
         public IActionResult Index()
         {
+            using PizzaContext context = new PizzaContext();
 
-            var pizzas = dbContext.Pizza 
-            return View();
+
+
+            List<Pizza> Pizze = context.Pizza.ToList();
+
+            return View("Index", Pizze);
+        }
+
+
+        public IActionResult Detail(int Id) {
+
+            using PizzaContext context = new PizzaContext();
+
+            Pizza? pizzaById = new Pizza();        
+
+                pizzaById = (from v in context.Pizza
+                        where v.Id == Id
+                        select v).FirstOrDefault();
+
+                if (pizzaById == null)
+                {
+                    return View("Detail", null);
+                }
+
+
+
+            return View("Detail", pizzaById);
         }
     }
 }
